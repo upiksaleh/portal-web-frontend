ebbce52bb8dd36de4b0d81f2b45430bb305949a1
upiksaleh <upxsal@gmail.com>
2023-06-21 20:03:03 +0800

    update resource-swr

--- libs/shared/api/src/lib/use-resource-swr.ts
+++ libs/shared/api/src/lib/use-resource-swr.ts
@@ -4,6 +4,24 @@ import qs from 'qs';
 import useSWR from 'swr';
 import { rightTrimSlashes } from '@portal-web/shared-base';
 
+const pathQueryStringBuild = (pathQuery) => {
+  return pathQuery?.length ? `/${pathQuery?.join('/')}` : '';
+}
+const paramsQueryStringBuild = (paramsQuery) => {
+  return paramsQuery ? `?${qs.stringify(paramsQuery)}` : '';
+}
+
+export const resourceUrlKeyBuild = <Key extends keyof DirectusItemsListType>({
+key,
+pathQuery,
+paramsQuery
+}: {key:Key}&ApiResourceProps<Key>) => {
+  const baseResourceUrl = rightTrimSlashes(
+    process.env.NEXT_PUBLIC_API_RESOURCE_BASE_URL ?? '/api/resources'
+  );
+  return `${baseResourceUrl}/${key}${pathQueryStringBuild(pathQuery)}${paramsQueryStringBuild(paramsQuery)}`;
+}
+
 export const resourceSwrFetcher = (key: string) =>
   fetch(key).then((r) => r.json());
 
@@ -14,14 +32,8 @@ export function useResourceSWR<Key extends keyof DirectusItemsListType>(
   }
 ) {
   const { pathQuery, paramsQuery, fetcher } = props;
-  const pathQueryString = pathQuery?.length ? `/${pathQuery?.join('/')}` : '';
-
-  const paramsQueryString = paramsQuery ? `?${qs.stringify(paramsQuery)}` : '';
-  const baseResourceUrl = rightTrimSlashes(
-    process.env.NEXT_PUBLIC_API_RESOURCE_BASE_URL ?? '/api/resources'
-  );
   return useSWR<DataListWithMeta>(
-    `${baseResourceUrl}/${key}${pathQueryString}${paramsQueryString}`,
+    resourceUrlKeyBuild({key,pathQuery,paramsQuery}),
     fetcher ?? resourceSwrFetcher
   );
 }
