8d7c8655308dcfcb4673988b1f7d41b2862fd1d8
upiksaleh <upxsal@gmail.com>
2023-06-23 18:25:57 +0800

    fix filename_disk

--- libs/shared/api/src/lib/server/helpers.ts
+++ libs/shared/api/src/lib/server/helpers.ts
@@ -1,7 +1,7 @@
 import { FileType } from '@directus/sdk';
 import { rightTrimSlashes, trimSlashes } from '@portal-web/shared-base';
 export const urlAssetCdn = (file: FileType | string) => {
-  const filename_disk = typeof file === 'string' ? file : file.filename_disk;
+  const filename_disk = typeof file === 'string' ? file : (file.filename_disk ?? `${file.fileId}.${file.extension}`);
   return `${process.env.NEXT_PUBLIC_CDN_BASE_URL}/portal-assets/live/${filename_disk}`;
 };
 
